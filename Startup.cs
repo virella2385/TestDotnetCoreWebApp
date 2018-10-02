c10cd989369f443d4a3230db084def80fc2f98a9
alakratt <alakratt@gmail.com>
2018-10-02 14:28:41 -0400

    Added interface

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 
 namespace TestNetCoreWebApp
 {
@@ -16,21 +11,21 @@ namespace TestNetCoreWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration conf)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter conf)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            string testvar = null;
             
 
             app.Run(async (context) =>
             {
-                var greeting = conf["Greeting"];
+                var greeting = conf.GetMessageOfTheDay();
                 await context.Response.WriteAsync(greeting);
             });
         }
